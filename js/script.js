d8022233efcb56bccd3355b9ba89ec14882b5145
Jason Li <jsn.lee1030@gmail.com>
2017-08-23 15:16:43 -0400

    Added tag property

--- js/script.js
+++ js/script.js
@@ -9,43 +9,51 @@ var html = '';
 // All the quotes containing the text of quote, source, citation, year
 var quotes = [
   {quote: 'The art of dining well is no slight art, the pleasure not a slight pleasure.',
-   source: 'Michel de Montaigne'
+   source: 'Michel de Montaigne',
+   tag: 'Art'
   },
   {quote: 'All I can say about life is, Oh God, enjoy it!',
-   source: 'Bob Newhart'
+   source: 'Bob Newhart',
+   tag: 'thoughts'
   },
   {quote: 'What\'s another word for Thesaurus?',
-   source: 'Steven Wright'
+   source: 'Steven Wright',
+   tag: 'philosophy'
   },
   {quote: 'Are you going to come quietly, or do I have to use earplugs?',
    source: 'Spike Milligan',
-   citation: '\"The Goon Show\"'
+   citation: '\"The Goon Show\"',
+   tag: 'humor'
   },
   {quote: 'You cannot fly like an eagle with the wings of a wren.',
    source: 'William Henry Hudson',
    citation: 'Afoot in England',
-   year: '1909'
+   year: '1909',
+   tag: 'philosophy'
   },
   {quote: 'If two men agree on everything, you may be sure that one of them is doing the thinking.',
    source: 'Lyndon B. Johnson'
   },
   {quote: 'Comedy is nothing more than tragedy deferred.',
    source: 'Pico Iyer',
-   citation: 'Time'
+   citation: 'Time',
+   tag: 'thoughts'
   },
   {quote: 'Success is meaningless if you can\'t sleep at night because of harsh things said, petty secrets sharpened against hard and stony regret, just waiting to be plunged into the soft underbelly of a \'friendship.\'',
    source: 'Margaret Cho',
    citation: 'Margaret Cho\'s Weblog',
-   year: '2006'
-  },
+   year: '2006',
+   tag: 'thoughts'
+  }
 ];
 
-
+// Add a help function print().
 function print(quote) {
   var outputDiv = document.getElementById('quote-box');
   outputDiv.innerHTML = quote;
-}
+};
 
+// The getRandomQuote() function
 function getRandomQuote() {
   var randomNumber = Math.floor( Math.random() * quotes.length );
   var splicedQuote = quotes.splice(randomNumber,1)[0];
@@ -54,11 +62,12 @@ function getRandomQuote() {
   if (quotes.length == 0) {
     quotes = viewedQuotes;
     viewedQuotes = [];
-  }
+  };
   return splicedQuote;
-}
+};
 
 
+// printQuote() function with html element using helper function print().
 function printQuote() {
   var selectedQuote = getRandomQuote();
 
@@ -69,13 +78,16 @@ function printQuote() {
     html += '<span class="citation">' + selectedQuote.citation + '</span>';
   } else {
     html += '';
-  }
+  };
   if( selectedQuote.year ) {
     html += '<span class="year">' + selectedQuote.year + '</span></p>'
   } else {
     html += '';
+  };
+  if( selectedQuote.tag ) {
+    html += '<p class="tag">' + selectedQuote.tag + '</p>'
+  } else {
+    html += '';
   }
   print(html);
-
-
 }
